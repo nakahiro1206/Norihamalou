77fca3d94fba440a59f52c757cab0e273df242d7
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2025-04-08 14:29:19 +0900

    useView

--- src/components/Greetings/Greetings.tsx
+++ src/components/Greetings/Greetings.tsx
@@ -3,8 +3,14 @@ import { Section } from "@/components/Section/Section";
 import Image from "next/image";
 import { css } from "@panda/css";
 import { Devider } from "../Devider/Devider";
+import { useInView } from "react-intersection-observer";
 
 export const Greetings = () => {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0.5,
+  });
+
   return (
     <Section id="greetings">
       <div
@@ -36,6 +42,7 @@ export const Greetings = () => {
             })}
           />
           <div
+            ref={ref}
             className={css({
               position: "absolute",
               rounded: "lg",
@@ -43,28 +50,36 @@ export const Greetings = () => {
               left: 0,
               right: 0,
               bottom: 0,
-              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              backgroundColor: inView ? "rgba(255, 255, 255, 0.5)" : "",
               padding: "1rem",
               display: "flex",
               flexDirection: "column",
               gap: "0.5rem",
               fontWeight: "bold",
+              animationName: inView ? "fadeIn" : "",
+              animationDuration: "0.5s",
+              animationTimingFunction: "ease-in-out",
+              animationFillMode: "forwards",
             })}
           >
-            <div
-              className={css({
-                width: "full",
-                textAlign: "center",
-              })}
-            >
-              {"ご挨拶"}
-            </div>
-            <Devider />
-            {`数々の名店の麻婆豆腐を食べ巡った経験を活かし、この数年間麻婆豆腐作りに熱中してきました。
+            {inView && (
+              <>
+                <div
+                  className={css({
+                    width: "full",
+                    textAlign: "center",
+                  })}
+                >
+                  {"ご挨拶"}
+                </div>
+                <Devider />
+                {`数々の名店の麻婆豆腐を食べ巡った経験を活かし、この数年間麻婆豆腐作りに熱中してきました。
     その集大成に相応しい究極の麻婆豆腐が出来たと自負しております。
 
     皆さまのご来店を心よりお待ちしております。
     ー 乘濵駿平`}
+              </>
+            )}
           </div>
         </div>
       </div>
