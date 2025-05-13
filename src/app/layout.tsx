cc34eb761c684f36861f07ef436242f01e63aa9d
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2025-05-13 19:25:55 +0900

    uncomment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
 					})}
 				>
 					<MenuProvider>
-						{/* <Overlay /> */}
+						<Overlay />
 						<FreezeWhileMenuOpen>
 							<Header />
 							<div
