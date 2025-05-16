629cbc9057d6756a9b0d0319bc4db9e8075ff30d
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2025-05-17 02:59:54 +0900

    ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export const metadata: Metadata = {
 	openGraph: {
 		title: "本格麻婆豆腐 乘濵楼",
 		description: "都内の麻婆豆腐を100軒以上食べめぐり、たどり着いた究極の麻婆。高級中華にも引けをとらない本気の味をご賞味あれ。",
+		url: "https://norihamaro.vercel.app/",
 		images: [
 			{
 				url: "https://norihamaro.vercel.app/ogp.png",
